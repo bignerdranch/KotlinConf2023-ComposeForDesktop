0772ee69a415f908acf05f061a09f80640672be2
bryansills <bsills@bignerdranch.com>
2023-04-12 15:10:53 +0200

    Provide Android specific implementations

--- core/src/androidMain/kotlin/com/bignerdranch/codapizza/core/ImageResource.android.kt
+++ core/src/androidMain/kotlin/com/bignerdranch/codapizza/core/ImageResource.android.kt
@@ -2,8 +2,20 @@ package com.bignerdranch.codapizza.core
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 
 @Composable
 actual fun getImageResource(imageResource: ImageResource): Painter {
-    TODO("Not yet implemented")
+    return painterResource(id = imageResource.resourceId)
 }
+
+private val ImageResource.resourceId: Int
+    get() = when (this) {
+        ImageResource.PizzaCrust -> R.drawable.pizza_crust
+        ImageResource.ToppingBasil -> R.drawable.topping_basil
+        ImageResource.ToppingMushroom -> R.drawable.topping_mushroom
+        ImageResource.ToppingOlive -> R.drawable.topping_olive
+        ImageResource.ToppingPepperoni -> R.drawable.topping_pepperoni
+        ImageResource.ToppingPeppers -> R.drawable.topping_peppers
+        ImageResource.ToppingPineapple -> R.drawable.topping_pineapple
+    }
--- core/src/androidMain/kotlin/com/bignerdranch/codapizza/core/StringResource.android.kt
+++ core/src/androidMain/kotlin/com/bignerdranch/codapizza/core/StringResource.android.kt
@@ -1,13 +1,35 @@
 package com.bignerdranch.codapizza.core
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource as androidStringResource
 
 @Composable
 actual fun getStringResource(stringResource: StringResource): String {
-    TODO("Not yet implemented")
+    return androidStringResource(stringResource.resourceId)
 }
 
 @Composable
 actual fun getStringResource(stringResource: StringResource, vararg formatArgs: Any): String {
-    TODO("Not yet implemented")
+    return androidStringResource(stringResource.resourceId, *formatArgs.toList().toTypedArray())
 }
+
+private val StringResource.resourceId: Int
+    get() {
+        return when (this) {
+            StringResource.AppName -> R.string.app_name
+            StringResource.PlaceOrderButton -> R.string.place_order_button
+            StringResource.OrderPlacedToast -> R.string.order_placed_toast
+            StringResource.PlacementPrompt -> R.string.placement_prompt
+            StringResource.PlacementNone -> R.string.placement_none
+            StringResource.PlacementLeft -> R.string.placement_left
+            StringResource.PlacementRight -> R.string.placement_right
+            StringResource.PlacementAll -> R.string.placement_all
+            StringResource.ToppingBasil -> R.string.topping_basil
+            StringResource.ToppingMushroom -> R.string.topping_mushroom
+            StringResource.ToppingOlive -> R.string.topping_olive
+            StringResource.ToppingPeppers -> R.string.topping_peppers
+            StringResource.ToppingPepperoni -> R.string.topping_pepperoni
+            StringResource.ToppingPineapple -> R.string.topping_pineapple
+            StringResource.PizzaPreview -> R.string.pizza_preview
+        }
+    }
